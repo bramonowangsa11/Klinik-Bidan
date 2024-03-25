2a99f1c5370bd8d6a798fcec5fb22caf80b994fb
bramonowangsa11 <bramonowangsa11@gmail.com>
2024-03-26 00:25:02 +0700

    update form

--- resources/views/layouts/daftar.blade.php
+++ resources/views/layouts/daftar.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.bootstrap')
 @section('content')
-<section class="vh-100" style="background:url({{asset('images/group3.jpg')}}); background-repeat:no-repeat; background-size: cover">
+<section class="vh-100" style="background:url({{asset('images/group3.jpg')}}); background-repeat:no-repeat; background-size: cover; background-attachment: scroll;">
   <div class="container h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100 ">
+    <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-lg-10 col-xl-9 ">
         {{-- card --}}
         <div class=" card small text-black" style="border-radius: 25px; background-color:rgb(229, 228, 228)">
@@ -14,28 +14,11 @@
             
           <div class="card-body p-md-1 ">
             
-            <div class="row justify-content-center">
-              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+            <div class="row">
+              <div class="col-md-4 col-lg-4 col-xl-4 order-2 order-lg-1">
 
                 {{-- <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p> --}}
-                <!-- pesan eror dan sukses -->
-                @if(Session::has('success'))
-                    <div class="alert alert-success">
-                        {{ Session::get('success') }}
-                    </div>
-                @endif
-                @if($errors->any())
-                  <div class="alert alert-danger">
-                    <ul>  
-                      @foreach($errors->all() as $error)
-                        <li>
-                          {{ $error }}
-                        </li>
-                      @endforeach
-                    </ul>
-                  </div>
-                @endif
-                <!-- akhir alert -->
+                
                 <form class="mx-1 mx-md-4 mt-0" method="POST" action="/daftar">
                   @csrf
                     {{-- nama lengkap --}}
@@ -43,7 +26,7 @@
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline border-bottom border-dark flex-fill mb-0">
                         <label class="form-label" for="form3Example1c">Nama Lengkap</label>
-                        <input type="text" id="form3Example1c" class="form-control" name="name" value="{{old('name')}}"/>
+                        <input style="background-color: rgb(229, 228, 228)" type="text" id="form3Example1c" class="form-control" name="name" value="{{old('name')}}"/>
                     </div>
                   </div>
                   {{-- NIK --}}
@@ -51,7 +34,7 @@
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline border-bottom border-dark flex-fill mb-0">
                         <label class="form-label" for="form3Example3c">NIK</label>
-                        <input type="text" id="form3Example3c" class="form-control" name="nik" value="{{old('nik')}}" />
+                        <input style="background-color: rgb(229, 228, 228)" type="text" id="form3Example3c" class="form-control" name="nik" value="{{old('nik')}}" />
                     </div>
                   </div>
                   {{-- ALamat --}}
@@ -59,7 +42,7 @@
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline border-bottom border-dark flex-fill mb-0">
                         <label class="form-label" for="form3Example4c">Alamat</label>
-                        <input type="text" id="form3Example4c" class="form-control" name="alamat" value="{{old('alamat')}}"/>
+                        <input style="background-color: rgb(229, 228, 228)" type="text" id="form3Example4c" class="form-control" name="alamat" value="{{old('alamat')}}"/>
                     </div>
                   </div>
                   {{-- Tanggal Lahir --}}
@@ -67,7 +50,7 @@
                     <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                     <div class="form-outline border-bottom border-dark flex-fill mb-0">
                         <label class="form-label" for="form3Example4cd">Tanggal Lahir</label>
-                        <input type="date" id="form3Example4cd" class="form-control" name="ttl" value="{{old('ttl')}}" />
+                        <input style="background-color: rgb(229, 228, 228)" type="date" id="form3Example4cd" class="form-control" name="ttl" value="{{old('ttl')}}" />
                     </div>
                   </div>
                   {{-- Nomor Telepon --}}
@@ -75,7 +58,7 @@
                     <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                     <div class="form-outline border-bottom border-dark flex-fill mb-0">
                         <label class="form-label" for="form3Example5cd">Nomor Telepon</label>
-                        <input type="text" id="form3Example5cd" class="form-control" name="no_telp" value="{{old('no_telp')}}"/>
+                        <input style="background-color: rgb(229, 228, 228)" type="text" id="form3Example5cd" class="form-control" name="no_telp" value="{{old('no_telp')}}"/>
                     </div>
                   </div>
                   {{-- Jenis Kelamin --}}
@@ -83,8 +66,8 @@
                     <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                         <div><label class="form-label" for="form3Example6cd">Jenis Kelamin</label></div>
-                         <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault1" value="Laki-Laki">
-                         <label class="form-check-label me-5" for="flexRadioDefault1">Laki-Laki</label>
+                         <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault1" value="Laki-Laki" checked>
+                         <label class="form-check-label me-3" for="flexRadioDefault1">Laki-Laki</label>
                          <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault2" value="Perempuan">
                          <label class="form-check-label" for="flexRadioDefault2">Perempuan</label>
                     </div>
@@ -103,14 +86,14 @@
                 {{-- </form> --}}
                 {{-- Bagian Kanan --}}
               </div>
-              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1 mt-lg-5">
+              <div class="col-md-10 col-lg-6 col-xl-4 order-2 order-lg-1 mt-lg-5">
                 {{-- <form class="mx-1 mx-md-4 mt-lg-5"> --}}
                 {{-- email --}}
                   <div class="d-flex flex-row align-items-center mb-2">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="border-bottom border-dark flex-fill mb-0 mt-lg-4">
                         <label class="form-label" for="form3Example7c">Email</label>
-                        <input type="email" id="form3Example7c" class="form-control" name="email" value="{{old('email')}}"  required  />
+                        <input style="background-color: rgb(229, 228, 228)" type="email" id="form3Example7c" class="form-control" name="email" value="{{old('email')}}"  required  />
                     </div>
                   </div>
                   {{-- Password --}}
@@ -118,7 +101,7 @@
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="border-bottom border-dark form-outline flex-fill mb-0">
                         <label class="form-label" for="form3Example4c">Password</label>
-                        <input type="password" id="form3Example4c" class="form-control" name="password" required />
+                        <input type="password" id="form3Example4c" class="form-control " name="password" style="background-color: rgb(229, 228, 228)" required />
                     </div>
                   </div>
                   <div class="d-flex flex-row align-items-center mb-2">
@@ -128,9 +111,28 @@
                     <button id="button-register" type="submit" class=" btn btn-sm border-0 col-2 mb-2">
                         <img src="{{asset('images/arrow.png')}}" alt="daftar" class=" ">
                     </button>
+                    <!-- pesan eror dan sukses -->
+                @if(Session::has('success'))
+                    <div class="alert alert-success">
+                        {{ Session::get('success') }}
+                    </div>
+                @endif
+                @if($errors->any())
+                  <div class="alert alert-danger">
+                    <ul>  
+                      @foreach($errors->all() as $error)
+                        <li>
+                          {{ $error }}
+                        </li>
+                      @endforeach
+                    </ul>
+                  </div>
+                @endif
+                <!-- akhir alert -->
                     {{-- <button type="button" class="btn btn-primary btn-lg">Register</button> --}}
                   </div>
                 </form>
+                
                 
 
                 {{-- <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid" alt="Sample image"> --}}
